c069fab930eccefafcac7aceadc4dae0a65d537e
Иван Лузиков <iluzikov@gmail.com>
2020-08-25 11:51:01 +0300

    Добавлена струкура MVC; Добавлен класс сотрудников

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeViewModel> _employees = new List<EmployeeViewModel>
+        {
+            new EmployeeViewModel
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SureName = "Иванов",
+                Patronymic = "Иванович",
+                Age = 35,
+                Position = "Начальник отдела"
+            },
+            new EmployeeViewModel
+            {
+                Id = 1,
+                FirstName = "Петр",
+                SureName = "Сидоров",
+                Patronymic = "Владимирович",
+                Age = 30,
+                Position = "Дизайнер"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            //return Content("Heelo from Controller");
+            return View();
+        }
+        public IActionResult Employees()
+        {
+            return View(_employees);
+        }
+    }
+}
---
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SureName { get; set; }
+        public string  Patronymic { get; set; }
+        public int Age { get; set; }
+        public string  Position { get; set; }
+    }
+}
