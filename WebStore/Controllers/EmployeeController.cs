507a1f3987421df080384848e993dfd17aeee539
Иван Лузиков <iluzikov@gmail.com>
2020-09-01 18:50:50 +0300

    добавил страницы: детализации и редавтирования

--- WebStore/Controllers/EmployeeController.cs
+++ WebStore/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -16,9 +17,70 @@ namespace WebStore.Controllers
             _employeesService = employeesService;
         }
 
+        /// <summary>
+        /// Выдает список всех сотрудников
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Employees()
         {
             return View(_employeesService.GetAll());
         }
+
+        /// <summary>
+        /// Детализация сотрудника по ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult EmployeeDetails(int id)
+        {
+            //Получаем сотрудника по Id
+            var employee = _employeesService.GetById(id);
+
+            //Если такого не существует
+            if (employee == null)
+                return NotFound(); // возвращаем результат 404 Not Found
+
+            //Иначе возвращаем сотрудника
+            return View(employee);
+        }
+    
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return View(new EmployeeViewModel());
+
+            var model = _employeesService.GetById(id.Value);
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            if (model.Id > 0) // если есть Id, то редактируем модель
+            {
+                var dbItem = _employeesService.GetById(model.Id);
+
+                if (ReferenceEquals(dbItem, null))
+                    return NotFound();// возвращаем результат 404 Not Found
+
+                dbItem.FirstName = model.FirstName;
+                dbItem.SurName = model.SurName;
+                dbItem.Age = model.Age;
+                dbItem.Patronymic = model.Patronymic;
+                dbItem.Position = model.Position;
+            }
+            else // иначе добавляем модель в список
+            {
+                _employeesService.AddNew(model);
+            }
+            _employeesService.Commit(); // станет актуальным позднее (когда добавим БД)
+
+            return RedirectToAction(nameof(Employees));
+        }
+
     }
 }
