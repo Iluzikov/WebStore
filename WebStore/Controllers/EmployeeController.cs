5781b5de8dd940d20735a514a11eae1da0cc46de
Иван Лузиков <iluzikov@gmail.com>
2020-09-01 18:01:41 +0300

    добавил отображение списка сотрудников

---
+++ WebStore/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeesService _employeesService;
+
+        public EmployeeController(IEmployeesService employeesService)
+        {
+            _employeesService = employeesService;
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employeesService.GetAll());
+        }
+    }
+}
