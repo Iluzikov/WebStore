8c8b13bb44690607f95cf440cb73bb3f20189f95
Иван Лузиков <iluzikov@gmail.com>
2020-09-02 19:00:11 +0300

    добавил методы удаления и редактирования списка автомобилей

--- WebStore/Controllers/CarController.cs
+++ WebStore/Controllers/CarController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -36,6 +37,53 @@ namespace WebStore.Controllers
                 return NotFound();
             return View(car);
         }
+
+        [HttpGet]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return View(new CarViewModel());
+
+            var model = _carsService.GetById(id.Value);
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(CarViewModel carModel)
+        {
+            if (carModel.Id > 0) // если есть Id, то редактируем модель
+            {
+                var dbItem = _carsService.GetById(carModel.Id);
+
+                if (ReferenceEquals(dbItem, null))
+                    return NotFound();// возвращаем результат 404 Not Found
+
+                dbItem.Brand = carModel.Brand;
+                dbItem.Model = carModel.Model;
+                dbItem.Engine = carModel.Engine;
+                dbItem.ReleaseYear = carModel.ReleaseYear;
+                dbItem.CarBody = carModel.CarBody;
+            }
+            else // иначе добавляем модель в список
+            {
+                _carsService.AddNew(carModel);
+            }
+            return RedirectToAction(nameof(Cars));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _carsService.Delete(id);
+
+            return RedirectToAction(nameof(Cars));
+        }
+
+
 
     }
 }
