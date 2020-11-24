f2b9665312a0f39088f2581fba7d576df87ccf15
Иван Лузиков <iluzikov@gmail.com>
2020-11-24 13:54:59 +0300

    Доработка базового клиента web api

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -2,10 +2,11 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WebStore.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected readonly string _serviceAddress;
         protected readonly HttpClient _client;
@@ -22,6 +23,49 @@ namespace WebStore.Clients.Base
                 }
             };
         }
+
+        //Get
+        public T Get<T>(string url) => GetAsync<T>(url).Result;
+        public async Task<T> GetAsync<T>(string url)
+        {
+            var response = await _client.GetAsync(url);
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+
+        //Post
+        public HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+        public async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await _client.PostAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        //Put
+        public HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+        public async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await _client.PutAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        //Delete
+        public HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+        public async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            var response = await _client.DeleteAsync(url);
+            return response;
+        }
+
+
+        #region Dispose
+
+        public void Dispose() => Dispose(true);
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+        }
+
+        #endregion
 
     }
 }
