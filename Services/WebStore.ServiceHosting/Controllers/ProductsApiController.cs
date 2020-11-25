74d3c1745270facf499e9898ada3016e8e4cd9ec
Иван Лузиков <iluzikov@gmail.com>
2020-11-25 08:22:00 +0300

    Добавил Api контроллер для каталога товаров

--- Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebStore.Domain;
+using WebStore.Domain.DTO.Products;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.ServiceHosting.Controllers
 {
     [Route(WebAPI.Products)]
     [ApiController]
-    public class ProductsApiController : ControllerBase
+    public class ProductsApiController : ControllerBase, IProductService
     {
+        private readonly IProductService _productService;
+        public ProductsApiController(IProductService productService) => _productService = productService;
 
+        [HttpGet("brands")]
+        public IEnumerable<BrandDTO> GetBrands() => _productService.GetBrands();
+
+        [HttpGet("categories")]
+        public IEnumerable<CategoryDTO> GetCategories() => _productService.GetCategories();
+
+        [HttpGet("{id}")]
+        public ProductDTO GetProductById(int id) => _productService.GetProductById(id);
+
+        [HttpPost]
+        public IEnumerable<ProductDTO> GetProducts([FromBody]ProductFilter filter = null) =>
+            _productService.GetProducts(filter ?? new ProductFilter());
     }
 }
