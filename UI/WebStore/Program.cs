cca55ef0e586429f89b88072c630c9345275da5c
Иван Лузиков <iluzikov@gmail.com>
2020-12-02 14:17:42 +0300

    Добавил библиотеку Serilog

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using WebStore.DAL;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using System;
 
 namespace WebStore
 {
@@ -20,8 +15,18 @@ namespace WebStore
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(host => host
                    .UseStartup<Startup>()
-                    //.ConfigureLogging(log => log
-                    //.AddConsole())
+                    .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(
+                        outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                    .WriteTo.RollingFile($@".\Log\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                    .WriteTo.File(new JsonFormatter(",", true), $@".\Log\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
+                    //.WriteTo.Seq("http://localhost:5341/")
+                    )
+                //.ConfigureLogging(log => log
+                //.AddConsole())
                 );
     }
 }
