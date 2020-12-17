ceaba7b72899642a88c97634e4e2e41b2d3a6dd2
Иван Лузиков <iluzikov@gmail.com>
2020-12-17 15:14:01 +0300

    Тестирование ненавязчивого AJAX

---
+++ UI/WebStore/ViewModels/AjaxTestDataViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public record AjaxTestDataViewModel(int? id, string message, DateTime serverTime);
+}
