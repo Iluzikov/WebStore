9d4ff942a380fad7a87224bbbdbd61939f9c1a39
Иван Лузиков <iluzikov@gmail.com>
2020-12-17 16:43:22 +0300

    пример реализации чата с использованием библиотеки SignalR

---
+++ UI/WebStore/Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebStore.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message) => await Clients.All.SendAsync("MessageFromClient", message);
+    }
+}
